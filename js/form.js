ec59c90f122c1aa107670e1f6ba115cb12c9280f
Bax08 <basseyeyo991@gmail.com>
2024-08-12 16:36:52 +0100

    test

--- js/form.js
+++ js/form.js
@@ -12,3 +12,24 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bookSeatButton1 = document.getElementById('bookSeatButton');
+    const bookSeatButton2 = document.getElementById('bookSeatButton2');
+    const floatingFormContainer = document.getElementById('floatingFormContainer');
+
+    function toggleForm() {
+        floatingFormContainer.classList.toggle('expanded');
+    }
+
+    bookSeatButton1.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default anchor behavior
+        toggleForm();
+    });
+
+    bookSeatButton2.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default anchor behavior
+        toggleForm();
+    });
+});
+
